f9c5d9946c8cd89d4497619625483636cd6d15d6
Ryo Numoto <ryonumoto@RyonoMacBook-Pro.local>
2020-08-16 17:45:32 +0900

    コメントの作成、一覧取得処理を実装

---
+++ app/controllers/comment_controller.rb
@@ -0,0 +1,20 @@
+class CommentController < ApplicationController
+  include ResponseMethods
+
+  def index
+    comments = Comment.all.map { |comment| { comment: comment.comment } }
+    response = { comments: comments }
+    render json: response
+  end
+
+  def create
+    anketo = Anketo.find(params[:anketo_id])
+    comment = anketo.comments.build(comment: params[:comment])
+
+    if comment.save
+      response_success
+    else
+      response_internal_server_error
+    end
+  end
+end
