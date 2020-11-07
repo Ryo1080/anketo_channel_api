1e988a6d5e15f8346bc7d25ce488176843fd9e9f
Ryo Numoto <ryonumoto@RyonoMacBook-Pro.local>
2020-11-07 14:39:10 +0900

    コントローラのロジックをmodelに切り出し

--- app/controllers/anketo_controller.rb
+++ app/controllers/anketo_controller.rb
@@ -2,52 +2,15 @@ class AnketoController < ApplicationController
   include ResponseMethods
 
   def search
-    anketos = Anketo.where('title LIKE ?',"%#{params[:keyword]}%")
-
-    case params[:sortId].to_i
-    when 0
-      anketos = anketos.order(created_at: :desc)
-    when 1
-      anketos = anketos
-        .left_joins(anketo_options: :votes)
-        .group("anketos.id")
-        .order(Arel.sql('count(votes.id) desc'))
-        .where(created_at: Time.now.months_ago(1).beginning_of_day...Time.now)
-    when 2
-      anketos = anketos
-        .left_joins(anketo_options: :votes)
-        .group("anketos.id")
-        .order(Arel.sql('count(votes.id) desc'))
-    end
-
-    anketos = anketos.where(category: params[:categoryId]) unless params[:categoryId].to_i == 0
-
-    render json: build_anketos_response(anketos)
+    render json: Anketo.build_search_response(
+      keyword: params[:keyword],
+      sort_id: params[:sortId],
+      category_id: params[:categoryId],
+    )
   end
 
   def show
-    anketo = Anketo.find(params[:id])
-
-    options = anketo.anketo_options
-    response = {
-      id: anketo.id,
-      title: anketo.title,
-      description: anketo.description,
-      image: anketo.image.to_s,
-      category: anketo.category,
-      options: []
-    }
-    options.each do |option|
-      response[:options].push(
-        {
-          id: option.id,
-          option: option.option,
-          votes: option.votes.size
-        }
-      )
-    end
-
-    render json: response
+    render json: Anketo.build_show_response(anketo_id: params[:id])
   end
 
   def create
@@ -74,25 +37,4 @@ class AnketoController < ApplicationController
   def destroy
     # TODO anketo削除処理
   end
-
-  private
-
-    def build_anketos_response(anketos)
-      vote_counts = AnketoOption.joins(:votes).group(:anketo_id).count('votes.id')
-
-      response = { anketos: []}
-      anketos.each do |anketo|
-        response[:anketos].push(
-          {
-            id: anketo.id,
-            title: anketo.title,
-            category: anketo.category,
-            image: anketo.image,
-            voteCount: vote_counts[anketo.id] || 0
-          }
-        )
-      end
-
-      response
-    end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,18 @@ class Comment < ApplicationRecord
     length: { maximum: 250 }
 
   belongs_to :anketo
+
+  class << self
+    def build_index_response(anketo: anketo)
+      comments = anketo.comments.order(created_at: :desc).map do |comment|
+        {
+          id: comment.id,
+          comment: comment.comment,
+          timestamp: comment.created_at.strftime("%Y年%m月%d日 %H:%M:%S"),
+        }
+      end
+
+      { comments: comments }
+    end
+  end
 end
